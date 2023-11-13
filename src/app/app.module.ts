c8eee3b8ca95b85d9d6c4497592fc91b84150e3c
Paris Kontoulis <parhs.97@gmail.com>
2023-11-13 13:10:39 +0200

    Add note card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NotesListComponent } from './pages/notes-list/notes-list.component';
 import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
 import { NoteCardComponent } from './note-card/note-card.component';
+import { NotesDetailsComponent } from './pages/notes-details/notes-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NotesListComponent,
     MainLayoutComponent,
-    NoteCardComponent
+    NoteCardComponent,
+    NotesDetailsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/note-card/note-card.component.ts
+++ src/app/note-card/note-card.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, Renderer2, Input } from '@angular/core';
 
 @Component({
   selector: 'app-note-card',
   templateUrl: './note-card.component.html',
-  styleUrls: ['./note-card.component.scss']
+  styleUrls: ['./note-card.component.scss'],
 })
 export class NoteCardComponent {
 
+  @Input() title: string;
+  @Input() body: string;
+
+  @ViewChild('truncator')
+  truncator: ElementRef<HTMLElement>;
+  @ViewChild('bodyText')
+  bodyText: ElementRef<HTMLElement>;
+  @ViewChild('paragraphText')
+  paragraphText: ElementRef<HTMLElement>;
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    //work out if there is a text overflow and if not, then hide the truncator
+    let style = window.getComputedStyle(this.paragraphText.nativeElement, null);
+    if (
+      this.paragraphText.nativeElement.scrollHeight >
+      this.bodyText.nativeElement.clientHeight
+    ) {
+      //if there is no text overflow, show the fade out truncator
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
+    } else {
+      //else (there is a text overflow, hide the fade out truncator)
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none');
+    }
+  }
 }
